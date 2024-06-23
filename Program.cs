06d0d965d94b18374b0d9357752cadde1e68cec8
Ruslan Kazankov <kazankovruslankrk@gmail.com>
2024-06-23 18:16:18 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TelegramChannelScipts
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
